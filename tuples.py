4dbb2d2f7350668aded54c648de1d4b37559d288
ZOYA AFREEN <zoyaafreen0007@gmail.com>
2026-09-07 21:52:38 +0530

    Implement nested_sum function for nested tuples
    Added a function to calculate the sum of elements in a nested tuple.

--- tuples.py
+++ tuples.py
@@ -296,11 +296,27 @@ t = (10, 5, 25, 8, 30, 15, 20)
 print(top_three(t))
 
 
+'''
+Nested Tuple Sum
+Write a function that accepts a nested tuple:
+
+t = ((1, 2), (3, 4), (5, 6))
+
+and returns the total sum of all elements.
+'''
+
+def nested_sum(t):
+    total = 0
+
+    for sub_tuple in t:
+        for num in sub_tuple:
+            total += num
+
+    return total
 
 
-
-
-
+t = ((1, 2), (3, 4), (5, 6))
+print(nested_sum(t))
 
 
 
